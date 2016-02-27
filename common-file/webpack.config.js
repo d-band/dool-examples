f7909593f83359cb50889428b04eeb4920b2665b
振震 <865702942@qq.com>
2016-02-27 22:06:28 +0800

    change webpack config

--- common-file/webpack.config.js
+++ common-file/webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = function(cfg, webpack) {
   var plugin = new webpack.optimize.CommonsChunkPlugin('common', 'common.js');
-  cfg.plugins = [plugin, ...cfg.plugins];
+  cfg.plugins.push(plugin);
   return cfg;
 };
